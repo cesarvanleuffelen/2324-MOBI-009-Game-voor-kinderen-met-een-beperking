834e917c00735589f613d587cc1ea933eec46d61
Toon Van Dyck <113974509+VanDyckToon@users.noreply.github.com>
2024-03-25 12:24:39 +0100

    Main Menu aangepast
    Het spel kan nu ook gestart worden door middel van de spatie knop.

--- Code/Main Menu.gd
+++ Code/Main Menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+func _ready():
+	set_process_input(true)
 
-
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_SPACE:
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
