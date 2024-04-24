ed8db8aad35a4fd93f4be8b33aca275c8fe73af1
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-04-24 11:15:57 +0200

    menu, settings changes & mute button
    - achtergrond bij pauze menu
    - mute knop voor muziek toegevoegd
    - bug fix: wanneer je van settings terug naar game gaat reset hij niets meer

--- Code/scenes/Menu.gd
+++ Code/scenes/Menu.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+var music_bus = AudioServer.get_bus_index("music")
 
 func _reset():
 	Global.totalClick = 5.0
@@ -10,22 +11,26 @@ func _reset():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if !Global.background:
-		$TextureRect.visible = false
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if !Global.background:
+		$TextureRect.visible = false
 
 
 func _on_pause_button_pressed():
 	$ColorRect.visible = true
+	$MarginContainer2.visible = true
+	$Label.visible = true
 	$VBoxContainer.visible = true
 
 
 func _on_hervat_pressed():
 	$ColorRect.visible = false
+	$MarginContainer2.visible = false
+	$Label.visible = false
 	$VBoxContainer.visible = false
 
 
@@ -35,4 +40,17 @@ func _on_stop_pressed():
 
 
 func _on_instellingen_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings_menu_ingame.tscn")
+	$Settings.visible = true
+
+
+func _on_mute_button_pressed():
+	if Global.mute:
+		Global.mute = false
+		AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+	else:
+		Global.mute = true
+		AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+
+
+func _on_back_pressed():
+	$Settings.visible = false
