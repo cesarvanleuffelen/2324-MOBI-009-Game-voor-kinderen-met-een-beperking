faac3b656d1427afcf1d4262200f7da7dbaa241e
NETbrew <113974723+cesarvanleuffelen@users.noreply.github.com>
2024-05-16 14:38:14 +0200

    bug fix & progress bar redesign
    - bug fix met toggle buttons
    - progress bar redesign

--- Code/ModeCheckBox.gd
+++ Code/ModeCheckBox.gd
@@ -3,7 +3,10 @@ extends CheckBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if Global.background:
+		button_pressed = true
+	else:
+		button_pressed = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,6 +14,8 @@ func _process(delta):
 	pass
 
 
-
-func _on_toggled(toggled_on):
-	Global.background = false;
+func _on_pressed():
+	if Global.background:
+		Global.background = false
+	else:
+		Global.background = true
--- Code/MusicCheckBox.gd
+++ Code/MusicCheckBox.gd
@@ -3,6 +3,20 @@ extends CheckBox
 var music_bus = AudioServer.get_bus_index("music")
 
 
+func _ready():
+	if Global.mute:
+		button_pressed = false
+	else:
+		button_pressed = true
 
 func _on_toggled(toggled_on):
 	AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+
+
+func _on_pressed():
+	if Global.mute:
+		Global.mute = false
+		AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+	else:
+		Global.mute = true
+		AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
